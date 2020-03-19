b8ddae3d2dd947d9d1f7faaf37c141ab99fcdc2d
Iulia Dragan <iuliadragan02@yahoo.com>
2020-03-19 11:39:30 -0700

    modif code in branch2

---
+++ src/lab1-master/src/loose/oose/fis/documents/ClasaNoua/ClasaNoua.java
@@ -0,0 +1,8 @@
+
+
+public class ClasaNoua  {
+    private String continut;
+}
+    public ClasaNoua(String continut) {
+        this.continut=continut
+    }
--- src/lab1-master/src/loose/oose/fis/documents/JSON.java
+++ src/lab1-master/src/loose/oose/fis/documents/JSON.java
@@ -24,6 +24,6 @@ public class JSON extends Document {
 
     @Override
     public String toString() {
-        return "JSON " + super.toString();
+        return "document JSON :" + super.toString();
     }
 }
